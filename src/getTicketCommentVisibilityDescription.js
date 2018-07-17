ae68227695b08fd5bc08107a40a1c72cf6a56984
Amy Boyd <amy@amyboyd.co.uk>
2018-07-17 18:52:46 +0100

    Change wording of getTicketCommentVisibilityDescription().
    Ticket: FF-88.

--- src/getTicketCommentVisibilityDescription.js
+++ src/getTicketCommentVisibilityDescription.js
@@ -5,16 +5,16 @@ function getTicketCommentVisibilityDescription(ticket, commentVisibility) {
 
     switch (commentVisibility) {
         case 'PUBLIC':
-            return 'Public comment (visible to everyone in the ' + (ticket.location ? 'location' : 'building') + ')';
+            return 'Public comment (visible to everyone)';
         case 'PRIVATE_TO_REPORTER':
             if (ticket.reporter) {
-                return 'Private comment (visible to ' + (ticket.reporter.name ? ticket.reporter.name : ticket.reporter.email) + ')';
+                return 'Private comment (visible to ' + (ticket.reporter.name ? ticket.reporter.name : ticket.reporter.email) + ' only)';
             } else {
                 return 'Private comment';
             }
             break;
         case 'INTERNAL_TO_TEAM':
-            return 'Internal comment (visible to your team)';
+            return 'Internal comment (visible to your team only)';
         default:
             throw new Error('Unsupported comment visibility: ' + commentVisibility);
     }
--- tests/getTicketCommentVisibilityDescription.spec.js
+++ tests/getTicketCommentVisibilityDescription.spec.js
@@ -5,22 +5,13 @@ const method = require('../dist/test-bundle.js').getTicketCommentVisibilityDescr
 
 describe('getTicketCommentVisibilityDescription', function() {
     describe('when the visibility is PUBLIC', function() {
-        it('if the ticket has a location, should say the comment is public within the location', function() {
+        it('should say the comment is visible to everyone', function() {
             const ticket = {
                 location: {
                     name: 'Room 4111',
                 },
             };
-            expect(method(ticket, 'PUBLIC')).to.equal('Public comment (visible to everyone in the location)');
-        });
-
-        it('if the ticket does not have a location, should say the comment is public within the building', function() {
-            const ticket = {
-                building: {
-                    name: 'WeWork',
-                },
-            };
-            expect(method(ticket, 'PUBLIC')).to.equal('Public comment (visible to everyone in the building)');
+            expect(method(ticket, 'PUBLIC')).to.equal('Public comment (visible to everyone)');
         });
     });
 
@@ -31,7 +22,7 @@ describe('getTicketCommentVisibilityDescription', function() {
                     name: 'Bob',
                 },
             };
-            expect(method(ticket, 'PRIVATE_TO_REPORTER')).to.equal('Private comment (visible to Bob)');
+            expect(method(ticket, 'PRIVATE_TO_REPORTER')).to.equal('Private comment (visible to Bob only)');
         });
 
         it('if the ticket has a reporter their email but no name, should use the reporter\'s email', function() {
@@ -40,13 +31,24 @@ describe('getTicketCommentVisibilityDescription', function() {
                     email: 'bob@example.test',
                 },
             };
-            expect(method(ticket, 'PRIVATE_TO_REPORTER')).to.equal('Private comment (visible to bob@example.test)');
+            expect(method(ticket, 'PRIVATE_TO_REPORTER')).to.equal('Private comment (visible to bob@example.test only)');
         });
 
         it('if the reporter is not available, should not fail', function() {
             const ticket = {
             };
             expect(method(ticket, 'PRIVATE_TO_REPORTER')).to.equal('Private comment');
+        });
+    });
+
+    describe('when the visibility is INTERNAL_TO_TEAM', function() {
+        it('should say the comment is visible to the team only', function() {
+            const ticket = {
+                reporter: {
+                    name: 'Bob',
+                },
+            };
+            expect(method(ticket, 'INTERNAL_TO_TEAM')).to.equal('Internal comment (visible to your team only)');
         });
     });
 
