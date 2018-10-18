704d07ac8e6bd1ba4de88c4bf23949455d02d01d
Amy Boyd <amy@amyboyd.co.uk>
2018-10-18 15:07:40 +0100

    Add to core colors: priority low/medium/high.

--- src/coreColors.js
+++ src/coreColors.js
@@ -1,11 +1,9 @@
-const blue = '#4463f7';
-const pink = '#eb1e4e';
-const green = '#1bd3a8';
-const orange = '#ffbd23';
-
 export default {
-    blue,
-    pink,
-    green,
-    orange,
+    blue: '#4463f7',
+    pink: '#eb1e4e',
+    green: '#1bd3a8',
+    orange: '#ffbd23',
+    priorityLow: '#58d786',
+    priorityMedium: '#ffbc4d',
+    priorityHigh: '#ff4c4e',
 };
--- tests/coreColors.spec.js
+++ tests/coreColors.spec.js
@@ -6,6 +6,7 @@ const coreColors = require('../dist/test-bundle.js').coreColors;
 describe('coreColors', function() {
     it('should have the colors', function() {
         expect(coreColors.blue).to.equal('#4463f7');
-        expect(Object.keys(coreColors).length).to.equal(4);
+        expect(coreColors.priorityHigh).to.equal('#ff4c4e');
+        expect(Object.keys(coreColors).length).to.equal(7);
     });
 });
